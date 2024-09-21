0ea718caf1f901d9a34a0098250037490614d9fd
JaninduDiz <janindu001@gmail.com>
2024-09-21 23:02:25 +0530

    add logout from user context

--- frontend/src/services/AuthServices.js
+++ frontend/src/services/AuthServices.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
 
-export const Register = async (firstName, lastName, email, password, phone) =>{
-    return await axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/register`,{
-        firstName,
-        lastName,
-        email,
-        password,
-        phone
-    });
+export const Login = (email, password) => {
+    return axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/login`, { email, password });
 };
 
-export const Login = async (email, password)=>{
-    return await axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/login`,{
-        email,
-        password
-    });
+export const Register = (firstName, lastName, email, password, phone, address) => {
+    return axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/register`, { firstName, lastName, email, password, phone, address });
 };
