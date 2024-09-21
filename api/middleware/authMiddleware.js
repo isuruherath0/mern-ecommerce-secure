b8bb97989b859623319177c19a25f39c3f8402e5
JaninduDiz <janindu001@gmail.com>
2024-09-21 23:54:02 +0530

    add authMiddleware

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-function authMiddleware(req, res, next) {
-    const token = req.header('Authorization');
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
+        return res.status(401).json({ status: 'failed', error: 'No token, authorization denied' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
         next();
-    } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+    } catch (error) {
+        res.status(401).json({ status: 'failed', error: 'Token is not valid' });
     }
-}
+};
 
 module.exports = authMiddleware;
