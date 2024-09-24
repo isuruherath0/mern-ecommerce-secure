0e0e10b3123ea845f3e62f31db611bd98b8b9cba
ThushanV <87628351+thushanvithana@users.noreply.github.com>
2024-09-24 13:06:55 +0530

    fx

--- frontend/src/validations/LoginValidations thu.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const LoginValidations = yup.object().shape({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(5).max(20)
-});
-
-export default LoginValidations;
